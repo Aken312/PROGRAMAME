96d2d485bec37cb8422bda0c32d20a6eb1b991a2
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2018-10-30 16:58:25 +0100

    adskjdsa

--- src/com/company/Programame111.java
+++ src/com/company/Programame111.java
@@ -1,7 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Programame111 {
     public static void main(String[] args) {
         // write your code here
+        Scanner sc = new Scanner(System.in);
+        int uvas = 999999;
+
+        while (uvas!=0) {
+            uvas = sc.nextInt();
+
+            int niveles = 0;
+            int totaluvas = 0;
+
+            while (uvas > totaluvas) {
+
+                niveles++;
+                totaluvas = niveles + totaluvas;
+            }
+
+            if (niveles !=0) {
+
+                System.out.println(niveles);
+
+            }
+
+
+
+
+        }
+
     }
 }
