1cd08f24da46472486e739c14cf34d2dbda98db5
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2018-11-13 16:11:02 +0100

    adskjdsa

---
+++ src/com/company/basic23.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class basic23 {
+
+    public static void main(String[] args) {
+        int aleatorio = (int) (Math.random() * 50)+1;
+        System.out.println(aleatorio);
+    }
+}
