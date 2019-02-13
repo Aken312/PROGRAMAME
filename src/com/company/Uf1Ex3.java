ec1708176dc2bdeb799af28e58844e1d94491337
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2019-02-13 20:12:43 +0100

    adskjdsa

--- src/com/company/Uf1Ex3.java
+++ src/com/company/Uf1Ex3.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Uf1Ex3 {
 
     public static void main(String[] args) {
-	// write your code here
+        {
+                Scanner sc = new Scanner(System.in);
+                int guardado=sc.nextInt();
+                int guardado2=sc.nextInt();
+                int cont=0;
+                int cont2=0;
+                while(guardado!=0 && guardado2!=0){
+                    if(guardado>guardado2 || guardado<guardado2){
+                        cont++;
+                    }else{
+                        cont2++;
+                    }
+                    guardado=guardado2;
+                    guardado2=sc.nextInt();
+                }
+                if(cont!=0 && cont2==0){
+                    System.out.println("Correcto");
+                }else if(cont!=0 && cont!=0){
+                    System.out.println("Incorrecto");
+                }else if(cont==0 && cont2!=0){
+                    System.out.println("Incorrecto");
+                }
+
+        }
     }
 }
